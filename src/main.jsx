d8537b424656f810970ff779e00f333b081327bb
Dipanshu <dipanshu.patel.wwl@gmail.com>
2025-06-24 22:16:18 +0530

    use hashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom"
 import App from "./App.jsx";
 import "./index.css";
 import { AuthProvier } from "./contexts/auth.jsx";
 import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
-    
+
     <AuthProvier>
         <StrictMode>
-         <ToastContainer />
-         <App />
+            <ToastContainer />
+            <HashRouter>
+                <App />
+            </HashRouter>
         </StrictMode>
     </AuthProvier>
 )
