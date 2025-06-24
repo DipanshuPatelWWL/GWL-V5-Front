370755f545913c56db7405e1bccd25e0d204c25a
Dipanshu <dipanshu.patel.wwl@gmail.com>
2025-06-24 13:12:00 +0530

    add localhost

--- src/API/Api.jsx
+++ src/API/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://gwl-v4-back.onrender.com",
+  baseURL: "https://gwl-v4-back.onrender.com" || "http://localhost:4000/api",
   withCredentials: true,
 })
 export default API;
