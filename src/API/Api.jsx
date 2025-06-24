73be3a1c89f9f2df87d54fef46109a538296e656
Dipanshu <dipanshu.patel.wwl@gmail.com>
2025-06-24 10:53:56 +0530

    change the base url in API.jsx page

--- src/API/Api.jsx
+++ src/API/Api.jsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 
-const API=axios.create({
-   baseURL: "http://localhost:4000/api",
+const API = axios.create({
+  baseURL: "https://gwl-v4-back.onrender.com/" || "http://localhost:4000/api",
   withCredentials: true,
 })
 export default API;
+
+
 
